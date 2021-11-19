3363aa897f058d10852667d6846de156f2dc2b66
sasha-kantoriz <no.money.no.honey5151@gmail.com>
2021-11-19 14:01:52 -0500

    Add navbar component

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,21 +1,22 @@
 <template>
-    <nav class="navbar navbar-dark">
+    <nav class="navbar">
         <div class="container">
             <div class="navbar" id="navbarSuppotedContent">
                 <ul class="nav nav-tabs ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#art" data-toggle="tab">Art</a>
+                        <a class="nav-link" href="#art">Art</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#tech" data-toggle="tab">Techn</a>
+                        <a class="nav-link" href="#tech">Tech</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#amend_service" data-toggle="tab">Amend Service</a>
+                        <a class="nav-link" href="#amend_service">[+]</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+
 </template>
 
 <script>
@@ -27,7 +28,19 @@ export default {
 <style scoped>
 .navbar {
     width: 100%;
-    justify-content: right;
+    justify-content: center;
 }
-
+.nav-tabs {
+    min-width: 80%;
+}
+li.nav-item {
+    width: 33.3%;
+}
+.nav-link {
+    min-height: 48px;
+    color: black;
+    font-weight: 500;
+    font-size: 1.1em;
+    font-family: monospace;
+}
 </style>
