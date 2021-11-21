e05e425fd27ca57176381abba5fbb15a59227408
sasha-kantoriz <no.money.no.honey5151@gmail.com>
2021-11-21 12:03:16 -0500

    Setup library components & views

---
+++ src/components/library.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-for book in >
+</template>
+
+<script>
+import book from "@/components/book.vue";
+
+export default {
+    name: 'library',
+    components: {
+        book
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
